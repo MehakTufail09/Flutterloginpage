cb4574d10acde73970dee0507b96c4814d42fdbe
Mehak <108197072+MehakTufail09@users.noreply.github.com>
2024-03-14 16:12:11 +0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import "login_page.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
